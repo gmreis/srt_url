c3beeafa43e695ce624aec1fef3b9cf5ec0e9304
Geraldo Mateus Reis <reis.geraldomateus@gmail.com>
2017-07-17 23:48:48 -0400

    Iniciar Cluster se numero CPUS maior que 1
    - Adicionado uma condição para utilizar Cluster somente se o numero de CPUs for maior que 1.

--- loader.js
+++ loader.js
@@ -1,25 +1,34 @@
-const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
-const http = require('http');
+if(numCPUs > 1) {
 
-if (cluster.isMaster) {
-  console.log('Master process is running');
+  const cluster = require('cluster');
 
-  // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+  if (cluster.isMaster) {
+    console.log('Master process is running');
+
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+      console.log('Starting a new worker');
+      cluster.fork();
+    });
+  } else {
+
+    const server = require('./config/server')
+    require('./config/database')
+    require('./config/routes')(server)
+
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
-    console.log('Starting a new worker');
-    cluster.fork();
-  });
 } else {
 
   const server = require('./config/server')
   require('./config/database')
   require('./config/routes')(server)
-  
+
 }
