f3b3c4b9a959ab973c15775d5bbbaba005706972
Geraldo Mateus Reis <reis.geraldomateus@gmail.com>
2017-07-17 23:07:12 -0400

    Adicionado Cluster
    - Adicionado no arquivo de inicialização, o modulo Cluster para sempre que iniciar o projeto, inicie varias instancias de acordo com o numero de núcleo do servidor.

--- loader.js
+++ loader.js
@@ -1,3 +1,25 @@
-const server = require('./config/server')
-require('./config/database')
-require('./config/routes')(server)
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+const http = require('http');
+
+if (cluster.isMaster) {
+  console.log('Master process is running');
+
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+
+  const server = require('./config/server')
+  require('./config/database')
+  require('./config/routes')(server)
+  
+}
